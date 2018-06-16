a52cfd40a495aa552d655590880358d1941ddb63
Matt Moore <mattmoor@google.com>
2018-06-16 19:17:22 +0000

    Stripped down version of the deep copy sample.

---
+++ generators/hello.go
@@ -0,0 +1,120 @@
+/*
+Copyright 2018 Matt Moore
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package generators
+
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+
+	"k8s.io/gengo/args"
+	"k8s.io/gengo/examples/set-gen/sets"
+	"k8s.io/gengo/generator"
+	"k8s.io/gengo/namer"
+	"k8s.io/gengo/types"
+
+	"github.com/golang/glog"
+)
+
+// These are the comment tags that carry parameters for hello generation.
+// TODO(mattmoor): Write something to parse these from comments.
+const (
+	// All of the tags we are sensitive too will have this root.
+	baseTagName = "hello:"
+
+	// Package tag for package-level configuration.
+	rootTagName = baseTagName + "package"
+
+	// Function tag for function-level configuration.
+	funcTagName = baseTagName + "function"
+
+	// Type; tag for type-level configuration.
+	typeTagName = baseTagName + "type"
+)
+
+// TODO: This is created only to reduce number of changes in a single PR.
+// Remove it and use PublicNamer instead.
+func theNamer() *namer.NameStrategy {
+	return &namer.NameStrategy{
+		Join: func(pre string, in []string, post string) string {
+			return strings.Join(in, "_")
+		},
+		PrependPackageNames: 1,
+	}
+}
+
+// NameSystems returns the name system used by the generators in this package.
+func NameSystems() namer.NameSystems {
+	return namer.NameSystems{
+		"public": theNamer(),
+		"raw":    namer.NewRawNamer("", nil),
+	}
+}
+
+// DefaultNameSystem returns the default name system for ordering the types to be
+// processed by the generators in this package.
+func DefaultNameSystem() string {
+	return "public"
+}
+
+func Packages(context *generator.Context, arguments *args.GeneratorArgs) generator.Packages {
+	boilerplate, err := arguments.LoadGoBoilerplate()
+	if err != nil {
+		glog.Fatalf("Failed loading boilerplate: %v", err)
+	}
+
+	inputs := sets.NewString(context.Inputs...)
+	packages := generator.Packages{}
+	header := append([]byte(fmt.Sprintf("// +build !%s\n\n", arguments.GeneratedBuildTag)), boilerplate...)
+
+	for i := range inputs {
+		glog.V(5).Infof("Considering pkg %q", i)
+		pkg := context.Universe[i]
+		if pkg == nil {
+			// If the input had no Go files, for example.
+			continue
+		}
+
+		// TODO(mattmoor): Handle package-level comments via:
+		// something(pkg.Comments)
+
+		for _, t := range pkg.Functions {
+			glog.V(5).Infof("  saw function %q", t.Name.String())
+		}
+
+		for _, t := range pkg.Types {
+			glog.V(5).Infof("  saw type %q", t.Name.String())
+		}
+
+		// TODO(mattmoor): Create generators to do things based on what we see.
+		packages = append(packages,
+			&generator.DefaultPackage{
+				PackageName: strings.Split(filepath.Base(pkg.Path), ".")[0],
+				PackagePath: pkg.Path,
+				HeaderText:  header,
+				GeneratorFunc: func(c *generator.Context) (generators []generator.Generator) {
+					return []generator.Generator{
+						// TODO(mattmoor): Set up the generator func.
+					}
+				},
+				FilterFunc: func(c *generator.Context, t *types.Type) bool {
+					return t.Name.Package == pkg.Path
+				},
+			})
+	}
+	return packages
+}
---
+++ main.go
@@ -0,0 +1,42 @@
+/*
+Copyright 2018 Matt Moore
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"k8s.io/gengo/args"
+
+	"github.com/golang/glog"
+
+	"github.com/mattmoor/hellogen/generators"
+)
+
+func main() {
+	arguments := args.Default()
+
+	// Override defaults.
+	arguments.OutputFileBaseName = "hello_generated"
+
+	// Run it.
+	if err := arguments.Execute(
+		generators.NameSystems(),
+		generators.DefaultNameSystem(),
+		generators.Packages,
+	); err != nil {
+		glog.Fatalf("Error: %v", err)
+	}
+	glog.Info("Completed successfully.")
+}
